e21781bb0ebc3004ef7cba4be42b2b181f770690
kishorvvn <chauhankishorh@gmail.com>
2023-07-26 16:20:18 -0400

    List -of-products-added

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 #MERN-Gujju's Treat
 Part 1
   Creat react app and initialize git repo
+Part 2 List products
+  1. create product array
+  2. ass product images
+  3. render products
+  4. style products
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div className="">
       <header className="">
-        <a href="">Gujju's Treat</a>
+        <a href="/">Gujju's Treat</a>
       </header>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+          {
+            data.products.map(product => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <p>
+                    {product.name}
+                  </p>
+                  <p><strong>${product.price}</strong></p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </main>
+
     </div>
   );
 }
