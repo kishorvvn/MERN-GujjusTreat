ab4a80ce3e9bdb255ede396d29aff60fdb6a1ae6
kishorvvn <chauhankishorh@gmail.com>
2023-07-26 17:05:04 -0400

    Server set up

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
 #MERN-Gujju's Treat
 Part 1
-  Creat react app and initialize git repo
+Creat react app and initialize git repo
+
 Part 2 List products
-  1. create product array
-  2. ass product images
-  3. render products
-  4. style products
+
+1. create product array
+2. ass product images
+3. render products
+4. style products
+
 Part 3 Routing
-  1. npm i react-router-dom
-  2. create route for home screen
-  3. create route for product screen
+
+1. npm i react-router-dom
+2. create route for home screen
+3. create route for product screen
+
+Part 4 Create Node.JS Server
+
+1. run npm init in root folder
+2. update package.json and set type: module
+3. Add .js to imports
+4. npm install express
+5. create server.js
+6. Add start command as node backend server.js
+7. require express
+8. create route for / and return backend is ready
+9. move product.js from fronend to backend
+10. create foute for /api/products
+11. return products
+12. run npm start
---
+++ backend/data.js
@@ -0,0 +1,66 @@
+const data = {
+  products: [
+    {
+      name: 'Nylon Khaman',
+      slug: 'nylon-khaman',
+      category: 'khaman',
+      image: '/images/Nylon-khaman.jpeg',
+      price: '10.00',
+      rating: 4.5,
+      numReviews: 10,
+      Description: 'Good Khaman'
+    },
+    {
+      name: 'Waati daal Khaman (plain)',
+      slug: 'waati-daal-khaman-plain',
+      category: 'khaman',
+      image: '/images/Watidal-khaman-plain.jpeg',
+      price: '10.00',
+      rating: 4.5,
+      numReviews: 10,
+      Description: 'Good Khaman'
+    },
+    {
+      name: 'Waati daal Khaman (tumtum)',
+      slug: 'waati-daal-khaman-tumtum',
+      category: 'khaman',
+      image: '/images/TumTum-khaman.jpeg',
+      price: '10.00',
+      rating: 4.5,
+      numReviews: 10,
+      Description: 'Good Khaman'
+    },
+    {
+      name: 'Waati daal Khaman (black pepper)',
+      slug: 'waati-daal-khaman-black-pepper',
+      category: 'khaman',
+      image: '/images/Black-pepper-khaman.jpeg',
+      price: '10.00',
+      rating: 4.5,
+      numReviews: 10,
+      Description: 'Good Khaman'
+    },
+    {
+      name: 'Sev Khamani',
+      slug: 'sev-khamani',
+      category: 'specials',
+      image: '/images/Sev-khamni.jpeg',
+      price: '12.00',
+      rating: 4.5,
+      numReviews: 10,
+      Description: 'Good sev khamani'
+    },
+    {
+      name: 'Khandvi',
+      slug: 'khandvi',
+      category: 'specials',
+      image: '/images/Khandvi.jpeg',
+      price: '12.00',
+      rating: 4.5,
+      numReviews: 10,
+      Description: 'Good Khandvi'
+    }
+  ]
+}
+
+export default data;
